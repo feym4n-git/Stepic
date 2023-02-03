4f3621001fb8725737d9d8b0cc6857549413ede4
Dmitriy Zemtsov <zemtsovdmitriy@gmail.com>
2023-02-03 12:52:58 +0700

    upd

--- OOP_Balakirev/4 Наследование и полиморфизм/4.1 Наследование в объектно-ориентированном программировании/4.1.6.py
+++ OOP_Balakirev/4 Наследование и полиморфизм/4.1 Наследование в объектно-ориентированном программировании/4.1.6.py
@@ -1,9 +1,11 @@
 class GenericView:
+
+
     def __init__(self, methods=('GET',)):
         self.methods = methods
 
     def get(self, request):
-        return ""
+        return ''
 
     def post(self, request):
         pass
@@ -11,11 +13,23 @@ class GenericView:
     def put(self, request):
         pass
 
-    def delete(self, request):
-        pass
+        def __getattribute__(self, item):
+            item = item.lower()
+            return object.__getattribute__(self, item)
 
+    class DetailView(GenericView):
+        def render_request(self, request, method):
+            if method not in self.methods:
+                raise TypeError('данный запрос не может быть выполнен')
 
-class DetailView(GenericView):
+            return getattr(self, method)(request)
 
-    def render_request(self, request, method):
-        pass
+        def get(self, request):
+            if type(request) is not dict:
+                raise TypeError('request не является словарем')
+
+            if 'url' not in request:
+                raise TypeError('request не содержит обязательного ключа url')
+
+            return f"url: {request['url']}"
+
