7b9c789ef5a179c2297b1dd1dae18a7087faa31f
Dmitriy Zemtsov <zemtsovdmitriy@gmail.com>
2023-02-03 12:35:02 +0700

    upd

---
+++ OOP_Balakirev/4 Наследование и полиморфизм/4.1 Наследование в объектно-ориентированном программировании/4.1.6.py
@@ -0,0 +1,21 @@
+class GenericView:
+    def __init__(self, methods=('GET',)):
+        self.methods = methods
+
+    def get(self, request):
+        return ""
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+
+class DetailView(GenericView):
+
+    def render_request(self, request, method):
+        pass
